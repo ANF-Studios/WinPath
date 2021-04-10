4d7de9b71d1eecd322c0ca20a9cba9f624c98178
ANF-Studios <theanfstudios@gmail.com>
2021-04-10 13:55:25 -0400

    Added initial tests project

---
+++ WinPath.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WinPath.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
