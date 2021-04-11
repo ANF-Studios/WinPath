bba2fcd8b5f2736700a1f48008b16597a352a447
ANF-Studios <theanfstudios@gmail.com>
2021-04-11 19:01:20 -0400

    Improved names and test

--- WinPath.Tests/PathTests.cs
+++ WinPath.Tests/PathTests.cs
@@ -4,23 +4,24 @@ using Xunit.Abstractions;
 
 namespace WinPath.Tests
 {
-    public class UnitTest1
+    public class PathTests
     {
         private readonly ITestOutputHelper output;
 
-        public UnitTest1(ITestOutputHelper output)
+        public PathTests(ITestOutputHelper output)
         {
             this.output = output;
         }
 
 
         [Fact]
-        public void Test1()
+        public void AddToUserPath()
         {
-            output.WriteLine("Hello World!");
             WinPath.Library.UserPath.AddToPath("foo", new Options { AddToUserVariables = true, });
             string path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
-            Xunit.Assert.True(path.EndsWith("foo;"));
+            bool isAddedToThePath = path.EndsWith("foo;");
+            output.WriteLine(isAddedToThePath ? "Variable is added to the path" : "Variable is NOT added to the path");
+            Xunit.Assert.True(isAddedToThePath);
         }
     }
 }
