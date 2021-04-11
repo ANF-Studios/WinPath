b701aca117813189442e17529493a551aef66278
ANF-Studios <theanfstudios@gmail.com>
2021-04-11 09:54:29 -0400

    Updated test1

--- WinPath.Tests/UnitTest1.cs
+++ WinPath.Tests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace WinPath.Tests
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+
         [Fact]
         public void Test1()
         {
-            Console.WriteLine("Hello World!");
+            output.WriteLine("Hello World!");
+            Xunit.Assert.True(true);
         }
     }
 }
