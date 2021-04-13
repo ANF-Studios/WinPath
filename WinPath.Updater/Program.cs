c4438ef78610bc369f65ed5185c90921fef7e1d0
ANF-Studios <theanfstudios@gmail.com>
2021-04-13 01:37:21 -0400

    Added WinPath.Updater project

---
+++ WinPath.Updater/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace WinPath.Updater
+{
+    class Program
+    {
+        private static readonly string executableDirectory = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\WinPath\\temp\\download\\WinPath.exe";
+        private static readonly string updateStatusFile = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\WinPath\\temp\\update\\status.txt";
+
+        public static void Main()
+        {
+            try
+            {
+                if (Environment.Is64BitOperatingSystem)
+                    File.Move(
+                        executableDirectory,
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.ProgramFiles
+                        )
+                        + "\\WinPath\\WinPath.exe",
+                        true
+                    );
+                else
+                    File.Move(
+                        executableDirectory,
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.ProgramFilesX86
+                        )
+                        + "\\WinPath\\WinPath.exe",
+                        true
+                    );
+                File.WriteAllText(updateStatusFile, string.Empty);
+                Console.WriteLine("WinPath is installed successfully!");
+                Environment.ExitCode = 0;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Could not install WinPath: " + exception.Message);
+                Environment.ExitCode = 1;
+            }
+            Environment.Exit(Environment.ExitCode);
+        }
+    }
+}
