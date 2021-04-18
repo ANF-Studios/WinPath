cba3d2b3712c94261f2df217fcc92f14392da4ea
ANF-Studios <theanfstudios@gmail.com>
2021-04-18 03:12:15 -0400

    Added unit tests for lib

---
+++ WinPath.Tests/WinPathLib.Tests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+using WinPath.Library;
+
+namespace WinPath.Tests
+{
+    public class LibraryTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public LibraryTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+        
+        [Fact]
+        [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+        public void AddToUserPath()
+        {
+            new UserPath().AddToPath("foo", false);
+
+            string path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
+            bool isAddedToThePath = path.EndsWith("foo;");
+            
+            output.WriteLine(isAddedToThePath ? "Variable is added to the path" : "Variable is NOT added to the path");
+            Assert.True(isAddedToThePath);
+        }
+
+        [Fact]
+        [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+        public void AddToUserPathWithBackup()
+        {
+            var userPath = new UserPath();
+            userPath.AddToPath("foo", true);
+
+            string path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
+            bool isAddedToThePath = path.EndsWith("foo;");
+
+            output.WriteLine(isAddedToThePath ? "Variable is added to the path" : "Variable is NOT added to the path");
+            Assert.True((isAddedToThePath && File.Exists(userPath.BackupDirectory + userPath.BackupFilename)));
+        }
+    }
+}
