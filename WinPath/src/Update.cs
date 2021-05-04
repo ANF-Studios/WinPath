c8195f2a5d1da15b66dad07c27278f663a8f38b8
ANF-Studios <theanfstudios@gmail.com>
2021-05-04 01:12:52 -0400

    Test code

--- WinPath/src/Update.cs
+++ WinPath/src/Update.cs
@@ -87,7 +87,7 @@ namespace WinPath
                     Console.WriteLine("Starting WinPath...");
                     var application = Process.Start(process);
                     //application.WaitForExit();
-                    System.Threading.Thread.Sleep(500);
+                    System.Threading.Thread.Sleep(5000);
                     processExitCode = application.ExitCode;
                     Console.WriteLine(application.ExitCode);
                 }
