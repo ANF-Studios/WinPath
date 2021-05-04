468aba9c29457d81e0483eadf999ac5929ca29cf
ANF-Studios <theanfstudios@gmail.com>
2021-05-04 02:32:05 -0400

    Set process exit code to 0

--- WinPath/src/Update.cs
+++ WinPath/src/Update.cs
@@ -91,6 +91,7 @@ namespace WinPath
                         const string installationPath = "C:\\Program Files\\WinPath\\";
                         Directory.CreateDirectory(installationPath);
                         File.Move(downloadDirectory + "\\WinPath_win10-x64.exe", installationPath + "WinPath.exe");
+                        processExitCode = 0;
                     }
                     else
                     {
