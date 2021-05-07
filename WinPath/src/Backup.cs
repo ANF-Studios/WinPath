ad08362ee5aaec31e84512bc1de018e521f40615
ANF-Studios <theanfstudios@gmail.com>
2021-05-07 09:09:37 -0400

    Remove unnecessary code

--- WinPath/src/Backup.cs
+++ WinPath/src/Backup.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace WinPath
 {
@@ -148,10 +145,6 @@ namespace WinPath
 
                     Console.WriteLine(seperator);
                     break;
-
-                //case HandleEventType.ListBackups:
-                //
-                //    break;
 
                 default:
                     throw new ArgumentException(
