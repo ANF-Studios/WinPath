20e943a8d3b840f013378849837088131012b143
ANF-Studios <theanfstudios@gmail.com>
2021-05-10 15:49:27 -0400

    Test commit

--- WinPath.Tests/BackupCommandTests.cs
+++ WinPath.Tests/BackupCommandTests.cs
@@ -201,7 +201,9 @@ namespace WinPath.Tests
                     "apply",
                     "--user",
                     "--name",
-                    backup
+                    backup,
+                    "--directory",
+                    "C:\\dev\\"
                 }
             );
         }
