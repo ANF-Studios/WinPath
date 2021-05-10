648d557ba49a0f66a3469ed71de4d58d45b4a797
ANF-Studios <theanfstudios@gmail.com>
2021-05-10 00:35:49 -0400

    Test commit

--- WinPath.Tests/UpdateTests.cs
+++ WinPath.Tests/UpdateTests.cs
@@ -18,7 +18,7 @@ namespace WinPath.Tests
         [Fact]
         public void WinPathDoesUpdate()
         {
-            Console.SetOut(new OutputRedirector(output));
+            Console.SetOut(null);
             Program.Main(
                 new string[]
                 {
@@ -40,7 +40,7 @@ namespace WinPath.Tests
         [Fact]
         public void WinPathIsInPath()
         {
-            Console.SetOut(new OutputRedirector(output));
+            Console.SetOut(null);
             var path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
             var isOnPath = path.Contains("%PROGRAMFILES%\\WinPath") || path.Contains("C:\\Program Files\\WinPath");
             output.WriteLine("WinPath is added to the Path: " + isOnPath);
