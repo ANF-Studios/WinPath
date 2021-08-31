aefdb9a8a5dd0a93014982b8d50a9fd72425c6f6
ANF-Studios <theanfstudios@gmail.com>
2021-08-31 00:01:51 -0400

    Fixes and imporvements to install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
 #Requires -RunAsAdministrator
+#Requires -Version 7.0
 
 using namespace System.Runtime.InteropServices;
 using namespace System.IO;
@@ -6,8 +7,8 @@ using namespace System.IO;
 if ($IsWindows -eq $false)
 {
     [Console]::WriteLine("You must be on Windows to run this script! Press any key to continue...");
-    [Console]::ReadKey();
-    [System.Environment]::Exit(-1);
+    [Console]::ReadKey() | Out-Null;
+    return;
 }
 
 $Is32or64BitOperatingSystem = ([RuntimeInformation]::OSArchitecture -eq [Architecture]::X64 -or [Architecture]::X86);
@@ -22,18 +23,18 @@ if ([System.Environment]::Is64BitOperatingSystem)
 {
     if (Test-Path -Path ([Path]::Combine($64BitOutput, "WinPath.exe")))
     {
-        [Console]::WriteLine("WinPath is already installed, run winpath update instead of this script!");
-        [Console]::ReadKey();
-        [System.Environment]::Exit(-1);
+        [Console]::Write("WinPath is already installed, run winpath update instead of this script!");
+        [Console]::ReadKey() | Out-Null;
+        return;
     }
 }
 else
 {
     if (Test-Path -Path ([Path]::Combine($32BitOutput, "WinPath.exe")))
     {
-        [Console]::WriteLine("WinPath is already installed, run winpath update instead of this script!");
-        [Console]::ReadKey();
-        [System.Environment]::Exit(-1);
+        [Console]::Write("WinPath is already installed, run winpath update instead of this script!");
+        [Console]::ReadKey() | Out-Null;
+        return;
     }
 }
 
@@ -45,37 +46,37 @@ try
         {
             if ([Environment]::OSVersion.Version.Major -eq 10)
             {
-                [Directory]::CreateDirectory($64BitOutput);
-                Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win10-x64.exe" -OutFile $64BitOutput;
-                $installed = true;
+                [Directory]::CreateDirectory($64BitOutput) | Out-Null;
+                Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win10-x64.exe" -OutFile ($64BitOutput + "WinPath.exe");
+                $installed = $true;
             }
             else
             {
-                [Directory]::CreateDirectory($64BitOutput);
-                Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win-x64.exe" -OutFile $64BitOutput;
-                $installed = true;
+                [Directory]::CreateDirectory($64BitOutput) | Out-Null;
+                Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win-x64.exe" -OutFile ($64BitOutput + "WinPath.exe");
+                $installed = $true;
             }
         }
         else # x86
         {
-            [Directory]::CreateDirectory($32BitOutput);
-            Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win-x86.exe" -OutFile $32BitOutput;
-            $installed = true;
+            [Directory]::CreateDirectory($32BitOutput) | Out-Null;
+            Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win-x86.exe" -OutFile ($32BitOutput + "WinPath.exe");
+            $installed = $true;
         }
     }
     else
     {
         if ([Environment]::Is64BitOperatingSystem) # Arm64
         {
-            [Directory]::CreateDirectory($64BitOutput);
-            Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win-arm64.exe" -OutFile $64BitOutput;
-            $installed = true;
+            [Directory]::CreateDirectory($64BitOutput) | Out-Null;
+            Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win-arm64.exe" -OutFile ($64BitOutput + "WinPath.exe");
+            $installed = $true;
         }
         else # Arm
         {
-            [Directory]::CreateDirectory($32BitOutput);
-            Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win-arm.exe" -OutFile $32BitOutput;
-            $installed = true;
+            [Directory]::CreateDirectory($32BitOutput) | Out-Null;
+            Invoke-WebRequest -Method "GET" -Headers $iwr_header -Uri "https://github.com/ANF-Studios/WinPath/releases/download/$version/WinPath_win-arm.exe" -OutFile ($32BitOutput + "WinPath.exe");
+            $installed = $true;
         }
     }
 
@@ -86,7 +87,7 @@ catch [System.Exception]
     $installed = $false;
     [Console]::WriteLine("An error occured while installing WinPath:");
     [Console]::WriteLine($_);
-    [Console]::ReadKey();
+    [Console]::ReadKey() | Out-Null;
 }
 
 if ($installed)
@@ -99,14 +100,14 @@ if ($installed)
 
     if ([Environment]::Is64BitOperatingSystem)
     {
-        [Environment]::SetEnvironmentVariable("Path", ("$path" + $64BitOutput), [System.EnvironmentVariableTarget]::Machine);
+        [Environment]::SetEnvironmentVariable("Path", ($path + $64BitOutput), [System.EnvironmentVariableTarget]::Machine);
     }
     else
     {
-        [Environment]::SetEnvironmentVariable("Path", ("$path" + $32BitOutput), [System.EnvironmentVariableTarget]::Machine);
+        [Environment]::SetEnvironmentVariable("Path", ($path + $32BitOutput), [System.EnvironmentVariableTarget]::Machine);
     }
     
-    [Console]::WriteLine("Congrats! WinPath is installed successfully! To ensure WinPath does work, restart your computer (optional).");
+    [Console]::Write("WinPath is installed successfully! To ensure WinPath does work, restart your computer (optional).");
     [Console]::ReadKey();
-    [Environment]::Exit(0);
+    return;
 }
